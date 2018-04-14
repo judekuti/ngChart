51b75185bda80f834d3f53ec189f7392dc2f3ecf
Jude Kuti <judekuti@gmail.com>
2018-04-14 16:10:31 -0500

    Import HttpClient and inject in constructor then create dailyForcast() to get the weather api

--- charts/src/app/weather.service.ts
+++ charts/src/app/weather.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class WeatherService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  dailyForecast(){
+    return this._http.get("").map(result => result);
+  }
 
 }
