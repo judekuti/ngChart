6f2dbf1748ccc2721f61039dd739a4ce49cffcb8
Jude Kuti <judekuti@gmail.com>
2018-04-15 00:19:51 -0500

    Create canvas with chart, retrieve data from api and include in chart canvas

--- charts/src/app/weather.service.ts
+++ charts/src/app/weather.service.ts
@@ -2,14 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'access'
+  })
+};
 
 @Injectable()
 export class WeatherService {
 
   constructor(private _http: HttpClient) { }
 
-  dailyForecast(){
-    return this._http.get("").map(result => result);
+  dailyForecast() {
+    return this._http.get("http://samples.openweathermap.org/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22").map(result => result);
   }
 
 }
